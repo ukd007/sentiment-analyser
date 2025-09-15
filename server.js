fbad0d89474435c27fe48004402d9af6197db29f
ukd007 <udayendukrdas@gmail.com>
2025-09-15 22:34:26 +0530

    idk

---
+++ server.js
@@ -0,0 +1,23 @@
+//Requirements
+const express = require("express");
+const path = require("path");
+const app = express();
+
+//Express Configuration
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const PORT = process.env.PORT || 8080;
+
+// Middleware to serve static files (CSS, JS, images)
+app.use(express.static(path.join(__dirname, "public")));
+
+//Routes
+app.get("/", (req, res) => {
+  res.render("user/home");
+});
+
+// Start server
+app.listen(8080, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
